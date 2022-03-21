410b4918235de7a70744bdc22878bc4f8f2872c2
Edgar Sargsyan <92edgarsargsyan@gmail.com>
2022-03-21 17:59:18 +0400

    feat: add simple web client

---
+++ web/src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+	production: false,
+	region: 'us-east-1',
+	userPoolId: 'us-east-1_TjF9YvuRw',
+	userPoolWebClientId: '6rh7i6sj4ssrbrf53np3rkbvld',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+import 'zone.js/dist/zone-error'; // Included with Angular CLI.
